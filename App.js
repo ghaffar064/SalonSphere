b6262914376b27e096c536ba6a8e9788959ece8e
ShahbazGurmani <shahbazgurmani786@gmail.com>
2024-03-19 13:52:38 -0700

    shahbaz

--- App.js
+++ App.js
@@ -4,12 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-
-
       <Text>Shayan</Text>
-
-     
-
+      <Text>Shayan 123</Text>
 
       <StatusBar style="auto" />
     </View>
