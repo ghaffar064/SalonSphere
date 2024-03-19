91424d8a568510b68a443915fe8ab7ad7e16b37b
ghaffar064 <ghaffar064@gmail.com>
2024-03-20 01:49:51 +0500

    changed by ghaffar

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-
-      <Text>Shayan</Text>
+      <Text>Salon Sphere by ghaffar</Text>
 
      
-
 
       <StatusBar style="auto" />
     </View>
@@ -19,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
